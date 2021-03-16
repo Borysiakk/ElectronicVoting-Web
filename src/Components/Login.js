a61b436ab23cc67924ef1830ba1100aeb858f820
Borysiak <szymaborys@gmail.com>
2021-03-17 00:46:00 +0100

    Token,KeyPublic save in localStorage

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,17 +1,37 @@
 
 import "./Login.css"
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
+import {IdentityService} from "../Services/IdentityService";
 
 export const Login = () =>
 {
+    const [login,setLogin] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleLoginVerification = () =>
+    {
+        IdentityService.Login(login,password).then(data=>
+        {
+            if(data.success)
+            {
+                localStorage.setItem('token',data.token);
+                localStorage.setItem('KeyPublic.n',data.keyPublic.n);
+                localStorage.setItem('KeyPublic.r',data.keyPublic.r);
+                localStorage.setItem('KeyPublic.g',data.keyPublic.g);
+            }
+            else
+            {
+                alert(JSON.stringify(data.errors))
+            }
+        });
+    }
+
     return <>
         <div className={"box"}>
-            <form>
                 <h1>Login</h1>
-                <input type={"text"} name={""} placeholder={"Username"}/>
-                <input type={"password"} name={""} placeholder={"Password"}/>
-                <input type={"submit"} name={""} value={"Login"}/>
-            </form>
+                <input onChange={e => setLogin(e.target.value)} type={"text"} name={""} placeholder={"Username"}/>
+                <input onChange={e => setPassword(e.target.value)} type={"password"} name={""} placeholder={"Password"}/>
+                <input type={"submit"} onClick={handleLoginVerification} name={""} value={"Login"}/>
         </div>
     </>
 }
--- src/Services/IdentityService.js
+++ src/Services/IdentityService.js
@@ -7,21 +7,21 @@ export const IdentityService =
 
 function Login(login,password)
 {
-    return fetch('https://localhost:5001/api/VotingIdentity/Login"',
+    return fetch('https://localhost:5001/api/VotingIdentity/Login',
+{
+        method: 'POST',
+        headers:
         {
-            method: 'POST',
-            headers:
-            {
-                'Accept': 'application/json; charset=utf-8',
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({Email:login,Password:password})
-        })
-        .then(response => response.json())
-        .then(data =>
-        {
-            return data;
-        });
+            'Accept': 'application/json; charset=utf-8',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({Email:login,Password:password})
+    })
+    .then(response => response.json())
+    .then(data =>
+    {
+        return data;
+    });
 }
 
 function Register()
